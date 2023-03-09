411e3fba7b7735433b8156ddd371c78ba05745ed
Dani <danielalinodasilvaoliveira@gmail.com>
2023-03-09 16:48:21 -0300

    finalizando atividade referente ao json, faltando somente duas questões - 09/03/2023

--- AULA-07/exercicio-json/modulo/estados_cidades_brasil.js
+++ AULA-07/exercicio-json/modulo/estados_cidades_brasil.js
@@ -5,12 +5,139 @@
  * Data: 03/03/2023
  * Versão: 1.0
  **********************************************************************************************/
-var  estadosCidades = require('../estados_cidades.js')
+var estadosBrasil = require('../estados_cidades.js')
+const { estadosCidades } = require('../estados_cidades.js')
 
-    const estadosJson = estadosCidades.estadosCidades.estados
+const estadosJson = estadosBrasil.estadosCidades.estados
 
-const getListaDeEstados = function(estados){
+const getListaDeEstados = function (json) {
+    const estadosJson = json.slice();
+    const siglasJson = {};
+    const ufList = [];
+    let status;
 
-    console.log(estadosJson[1])
+    if (estadosJson !== undefined) {
+        estadosJson.forEach(estadosSiglas => {
+            ufList.push(estadosSiglas.sigla);
+            siglasJson.UF = ufList;
+            siglasJson.quantidade = ufList.length;
+        });
+        status = true;
+    } else {
+        status = false;
+    }
+
+    if (status) {
+        return siglasJson;
+    }
 }
-getListaDeEstados(estadosCidades)
+const getDadosEstado = function (json, filtro) {
+    const uf = filtro;
+    const estadoJson = json.slice()
+    const dadosEstadoJson = {};
+    let status;
+
+    if (uf !== undefined) {
+
+        estadoJson.forEach(dados => {
+
+            if (dados.sigla === uf) {
+                dadosEstadoJson.uf = dados.sigla;
+                dadosEstadoJson.descricao = dados.nome;
+                dadosEstadoJson.capital = dados.capital;
+                dadosEstadoJson.regiao = dados.regiao;
+            }
+        });
+
+        status = true
+    } else {
+        status = false
+    }
+
+    if (status) {
+        return dadosEstadoJson;
+
+    } else {
+        console.log('ERRO');
+        return false;
+    }
+}
+const getCapitalEstado = function (json, filtro) {
+    const uf = filtro;
+    const estadoJson = json.slice();
+    const capitalJson = {};
+    let status;
+
+    if (uf !== undefined) {
+        estadoJson.forEach(estado => {
+
+            if (estado.sigla === uf) {
+
+                capitalJson.uf = estado.sigla;
+                capitalJson.descricao = estado.nome;
+                capitalJson.capital = estado.capital;
+            }
+
+        });
+
+        status = true;
+
+    } else {
+        status = false;
+    }
+
+    if (status) {
+        return capitalJson;
+    } else {
+        return false;
+    }
+
+}
+const getEstadosRegiao = function (json, filtro) {
+    const regiaoFiltro = filtro;
+    const estadosJson = json.slice();
+    const regiaoJson = {};
+    const estadosList = [];
+    let status;
+
+    if (regiaoFiltro !== undefined) {
+
+        estadosJson.forEach(regioesEstados => {
+
+            if (regioesEstados.regiao == regiaoFiltro) {
+
+                regiaoJson.regiao = regioesEstados.regiao;
+                regiaoJson.estados = estadosList;
+
+                const estadosPorRegiao = {};
+
+                estadosPorRegiao.uf = regioesEstados.sigla;
+                estadosPorRegiao.descricao = regioesEstados.nome;
+
+                estadosList.push(estadosPorRegiao);
+
+            }
+        });
+        status = true;
+    } else {
+        status = false;
+    }
+
+    if (status) {
+        return regiaoJson;
+    } else {
+        return false;
+    }
+
+}
+const getCapitalPais = function (json) {
+    
+}
+const getCidades = function (json, filtro) {
+    
+}
+
+// getEstadosRegiao(estadosJson, 'Sudeste')
+// getCapitalEstado(estadosJson, 'AL')
+// getDadosEstado(estadosJson, 'SP')
+// getListaDeEstados(estadosJson)
